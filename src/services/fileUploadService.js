9943ecc31e70d7c28db9d1364c3af783d9318ce6
ayendisimeon1 <mrayendi1@gmail.com>
2025-08-21 12:26:40 +0100

    update

--- src/services/fileUploadService.js
+++ src/services/fileUploadService.js
@@ -5,8 +5,9 @@ const upload = multer({
   storage,
   limits: { fileSize: 2 * 1024 * 1024 }, // 2MB max
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === 'image/png') cb(null, true);
-    else cb(new AppError('Only PNG images are allowed', 400));
+    const allowedTypes = ['image/png', 'image/jpeg', 'image/jpg'];
+    if (allowedTypes.includes(file.mimetype)) cb(null, true);
+    else cb(new Error('Only PNG and JPG images are allowed', 400));
   },
 });
 
